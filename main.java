347fab53815e7b5b6abe36eba0b84975f6972651
xkwangcn <wangxinkecn@163.com>
2021-06-15 10:59:33 +0200

    update java code

--- main.java
+++ main.java
@@ -1,9 +1,8 @@
-class Demo
-{
-  public static void main(String[] args)
-      ｛
-            System.out.println("Hello Java");
-       ｝ 
-}
+// Your First Program
 
+class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
 
